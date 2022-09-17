39eedc7f1aabe29100f333a8a84de93e6ee984aa
pereira299 <lucasper057@gmail.com>
2022-09-17 19:03:32 -0300

    initial commit

---
+++ src/test/java/br/edu/utfpr/td/tsi/mybankproject/MyBankProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.utfpr.td.tsi.mybankproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyBankProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
